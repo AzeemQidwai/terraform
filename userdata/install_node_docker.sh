614ed4a474f2e12e98918a599e6acbb69401d932
10284324+AzeemQidwai@users.noreply.github.com <10284324+AzeemQidwai@users.noreply.github.com>
2024-05-05 01:41:00 +0500

    shell script updated

--- userdata/install_node_docker.sh
+++ userdata/install_node_docker.sh
@@ -1,22 +1,9 @@
 #!/bin/bash -i
 
-# Create .bashrc if it doesn't exist
-touch ~/.bashrc
+# This script updates system packages, installs Docker, Nginx, Node.js, and starts their services.
 
 # Update system packages
 sudo apt update -y
-
-# Install NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-
-# Source .bashrc to load NVM
-source ~/.bashrc
-
-# Install Node.js
-sudo apt install -y nodejs
-
-# Install Node.js version 20 using NVM
-nvm install 20
 
 # Install Docker and Nginx
 sudo apt install -y docker.io nginx
@@ -26,3 +13,7 @@ sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl start nginx
 sudo systemctl enable nginx
+
+# Install Node.js using NodeSource repository
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo bash -
+sudo apt-get install -y nodejs
